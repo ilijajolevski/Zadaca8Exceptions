b83a6a52a4b28fbcb504ef0278b0a6de77841ee8
Ilija Jolevski <ilija.jolevski@gmail.com>
2020-12-16 09:42:50 +0100

    Dodadena klasa Student vo drug package

--- src/fikt/edu/mk/Main.java
+++ src/fikt/edu/mk/Main.java
@@ -1,5 +1,7 @@
 package fikt.edu.mk;
 
+import organizacija.lugje.Student;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -14,5 +16,13 @@ public class Main {
             System.out.println("Ne mozam da napravam covek so tolku godini");
             System.out.println(greskata.toString());
         }
+
+        Student s;
+        try {
+             s = new Student(20,"ime", "prez", 99);
+        } catch (NevalidniGodiniException e) {
+            e.printStackTrace();
+        }
+
     }
 }
